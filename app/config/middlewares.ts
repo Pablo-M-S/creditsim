ad7c53b9a6d3b0495aa002809b9dcda70e4e2554
Pablo-M-S <munizp900@gmail.com>
2026-09-23 19:43:08 -0300

    Update middlewares.ts

--- app/config/middlewares.ts
+++ app/config/middlewares.ts
@@ -4,7 +4,13 @@ const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: '*',
+      headers: '*',
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
